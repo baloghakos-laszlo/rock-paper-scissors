06637a599c912de8599bb3a8966da4b610e6bebd
baloghakos-laszlo <baloghakos8@gmail.com>
2022-12-06 00:14:35 +0100

    Create game() function

--- script.js
+++ script.js
@@ -13,27 +13,67 @@ function getComputerChoice() {
 }
 
 
+let win
+let lose
 
 function playOneRound(playerSelection, computerSelection) {
-    let playerSelectionCleanString = playerSelection.toLowerCase()
+    
 
-    let message = (`You chose ${playerSelectionCleanString}. The computer chose ${computerSelection}.`)
-    console.log(message)
+    let playerSelectionCleanString = playerSelection.toLowerCase();
+
+    //if statement to check the input is valid
+    if ( !(options.includes(playerSelectionCleanString)) ){
+        console.log( "Are your sure you typed your choice correctly?");
+        return;
+    }
+
+    let message = (`You chose ${playerSelectionCleanString}. The computer chose ${computerSelection}.`);
+
     if (playerSelectionCleanString == computerSelection) {
-        return `${message} It's a tie!`
+        console.log(`${message} It's a tie!`);
+        
+
     }
 
     else if (
     (playerSelectionCleanString == "rock" && computerSelection == "scissors") ||
     (playerSelectionCleanString == "paper" && computerSelection == "rock") || 
     (playerSelectionCleanString == "scissors" && computerSelection == "rock") ) {
-        return `${message} You won!`
+        console.log( `${message} You won!`);
+        win++
     }
 
     else {
-        return `${message} You lost!`
+        console.log( `${message} You lost!`)
+        lose++
     }
 }
+
+function game(numGames = 5) {
+     win = 0
+     lose = 0
+
+    for (let i = 0; i < numGames; i++) {
+        let guess = prompt("Please enter your guess!");
+        playOneRound(guess, getComputerChoice());
+     }
+
+    console.log("Computer wins:" + lose)
+    console.log("Your wins:" +win)
+
+     if (win == lose) {
+        console.log("It's a tie!")
+     }
+
+    else if (win > lose) {
+        console.log("You beat the computer! Congratulations")        
+     }
+
+     else {
+        console.log("You lost! Try again next time.")
+     }
+}
+
 
     // switch (playerSelectionCleanString) {
     //     case "rock":
